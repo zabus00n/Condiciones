5159a0804b481527326c29f9d7e78a15bb4dbf33
Ruben Yojay <ruben.yojay@gmail.com>
2023-08-04 12:03:26 -0400

    [Problema-1] added html, css and js content

---
+++ Problema-1/assets/js/script.js
@@ -0,0 +1,11 @@
+let clickMe = document.querySelector("img");
+let imgBorder = false;
+clickMe.addEventListener("click", imageBorderState);
+function imageBorderState() {
+  if (!imgBorder) {
+    clickMe.style.border = "2px solid red";
+  } else {
+    clickMe.style.border = "none";
+  }
+  imgBorder = !imgBorder;
+}
